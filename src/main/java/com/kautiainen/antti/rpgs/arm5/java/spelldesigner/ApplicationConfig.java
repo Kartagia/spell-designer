06ddf025d0031e5d51ef6b3565b3ab6651bb27f9
Antti Kautiainen <antti@kautiainen.com>
2024-08-30 19:58:59 +0300

    feat: application ApplicationConfig
    The application performing the servlet.
    
    Changes:
    . new file:   src/main/java/com/kautiainen/antti/rpgs/arm5/java/spelldesigner/ApplicationConfig.java
      - feat: THe main application file.

---
+++ src/main/java/com/kautiainen/antti/rpgs/arm5/java/spelldesigner/ApplicationConfig.java
@@ -0,0 +1,17 @@
+package com.kautiainen.antti.rpgs.arm5.java.spelldesigner;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("arm5/spells")
+public class ApplicationConfig extends Application {
+    
+
+  @Override
+  public java.util.Set<Class<?>> getClasses() {
+    return super.getClasses();
+  }
+
+  
+
+}

9854de33f2dfd17d9045d53084542fe089747a9f
Antti Kautiainen <antti@kautiainen.com>
2024-08-31 14:24:23 +0300

    rename: mvoe SpellGuideline to spelldesigner.  Changes to be committed: -	renamed:    src/main/java/com/kautiainen/antti/rpgs/arm5/java/SpellGuideline.java -> src/main/java/com/kautiainen/antti/rpgs/arm5/java/spelldesigner/SpellGuideline.java

--- src/main/java/com/kautiainen/antti/rpgs/arm5/java/spelldesigner/SpellGuideline.java
+++ src/main/java/com/kautiainen/antti/rpgs/arm5/java/spelldesigner/SpellGuideline.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-package com.kautiainen.antti.rpgs.arm5.java;
+package com.kautiainen.antti.rpgs.arm5.java.spelldesigner;
 
 /**
  * A class representing a spell guideline.
